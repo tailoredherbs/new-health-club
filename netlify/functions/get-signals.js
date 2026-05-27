f2e9077e6c5831ee34dc9475fbd05da100b66c47
tailoredherbs <jakob.rohrauer@gmail.com>
2026-05-27 15:28:57 +0800

    Update get-signals.js

--- netlify/functions/get-signals.js
+++ netlify/functions/get-signals.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-exports.handler = async function() {
+export const handler = async function() {
   try {
-    const data = fs.readFileSync(path.join(process.cwd(), 'signals-data.json'), 'utf8');
+    const data = readFileSync(join(process.cwd(), 'signals-data.json'), 'utf8');
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
@@ -14,28 +14,6 @@ exports.handler = async function() {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
       body: '[]'
-    };
-  }
-};        location: fm.location || '',
-        date: fm.date || '',
-        tag: fm.tag || '',
-        description: fm.description || '',
-        image: fm.image || ''
-      };
-    });
-
-    signals.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify(signals)
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ error: err.message })
     };
   }
 };
